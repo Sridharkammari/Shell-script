fa9cb032422fa7ae99b62058aaf44115c952d7c3
Sreedhar <ksridhar757@gmail.com>
2025-01-01 20:17:12 +0530

    normal changes

--- Helloworld.sh
+++ Helloworld.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-#This line is commented 
+# A simple script to greet the user and show the date
 
-echo "Hello World"
+# Greet the user
+echo "Hello, $(whoami)! Welcome to Shell Scripting."
+
+# Display the current date and time
+echo "Today's date is: $(date)"
+
+# Show the current working directory
+echo "You are currently in: $(pwd)"
+
+# End message
+echo "Have a great day!"
