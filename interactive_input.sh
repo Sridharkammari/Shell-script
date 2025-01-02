58830450bd41b12f0b64904e83ade87dfcb6e64d
sreedhar <ksridhar757@gmail.com>
2025-01-02 21:59:40 +0530

    this

---
+++ interactive_input.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+#Prompting user for input
+echo "Enter your name:"
+
+read Name
+
+echo "Enter your age:"
+
+read Age
+
+#output the values entered bu user
+
+eccho "Hello $Name, you are $Age years old"
