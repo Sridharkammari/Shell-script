fdf95be15e4d1e1ba43eeaeee13e9a06521ba4da
Sreedhar <ksridhar757@gmail.com>
2025-01-13 22:48:09 +0530

    theisi

---
+++ studentdetail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "Enter student Name:" Name
+read -p "Enter student Rollno:" Rollno
+read -p "Enter Student totalmarks:" Markes
+
+echo "Please find the below student detais:"
+echo "---------------------------"
+echo "The studentName is":${Name}
+echo "THe student Rollno is:"${Rollno}
+echo "THe total marks is :"${Markes}
