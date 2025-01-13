9f7565e3535c5d35d9915c6e4f471b2982557e3a
Sreedhar <ksridhar757@gmail.com>
2025-01-13 19:02:02 +0530

    the read file

--- Read.sh
+++ Read.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "enter A value:"
 read A
-echo "enter B balue:"
+echo "enter B value:"
 read B
-echo "the value of A is $A and the value of B is $B"
+echo "enter C value:"
+read C
+echo "the value of A is $A and the value of B is $B and the value of C is $C"
 
