c2917beaf154dbb4c86c508ac3ef7fde9e663e41
Sreedhar <ksridhar757@gmail.com>
2025-01-13 22:50:35 +0530

    theisi

--- studentdetail.sh
+++ studentdetail.sh
@@ -5,6 +5,6 @@ read -p "Enter Student totalmarks:" Markes
 
 echo "Please find the below student detais:"
 echo "---------------------------"
-echo "The studentName is":${Name} | wc -c
+echo "The studentName is":$({Name} | wc -c)
 echo "THe student Rollno is:"${Rollno}
 echo "THe total marks is :"${Markes}
