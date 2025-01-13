f15e159d0346ae3b941ce475b31b8616dcdc0f17
Sreedhar <ksridhar757@gmail.com>
2025-01-13 21:14:42 +0530

    newline

--- Read.sh
+++ Read.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "enter A value:"
+echo  -n "enter A value:"
 read A
-echo "enter B value:"
+echo -n "enter B value:"
 read B
-echo "enter C value:"
+echo -n "enter C value:"
 read C
 echo "the value of A is $A and the value of B is $B and the value of C is $C"
 
