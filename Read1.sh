cc434d3e66e950770c4ea48175b556ba8dbc6b2f
Sreedhar <ksridhar757@gmail.com>
2025-01-13 21:40:35 +0530

    theisi

--- Read1.sh
+++ Read1.sh
@@ -3,3 +3,8 @@ Read -p "Enter A Value:" A
 Read -p "Enter B Value:" B
 Read -p "Enter C Value:" C
 Read -p "Enter D Value:" D
+
+echo "The A values is:$A"
+echo "The B values is:$B"
+echo "The c values is:$C"
+echo "The D values is:$D"
