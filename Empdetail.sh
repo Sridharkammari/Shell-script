de67c06e62a94ac9f1c76f20fd59e298cdd14fab
Sreedhar <ksridhar757@gmail.com>
2025-01-13 23:01:17 +0530

    theisi

---
+++ Empdetail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -p "Enter Emp Name:" Ename
+read -p "Enter Empno:" Eno.
+read -p "Enter Emp address:" Empaddr
+read -p "Enter Emp salary:" Esal
+
+echo "The Employee details are"
+echo"$Ename:$Eno:$Empaddr:$Esal"
+echo"$Ename:$Eno:$Empaddr:$Esal">> Emp.txt
