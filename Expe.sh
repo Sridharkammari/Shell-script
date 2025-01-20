99a3e35f7225e43237abb054d1eaa5f34ddd858a
Sreedhar <ksridhar757@gmail.com>
2025-01-20 10:09:20 +0530

    this is commit

--- Expe.sh
+++ Expe.sh
@@ -1,5 +1,7 @@
-#!bin/bash
-read -p "enter the first number:" A
-read -p "enter the second number:" B
-sum = `expr $A + $B` 
-echo "the total sum of two number is:$sum"
+#! /bin/bash
+read -p "Enter First Number:" a
+read -p "Enter First Number:" b
+sum=`expr $a + $b`
+echo "The Sum: $sum"
+sum=$(expr $a + $b)
+echo "The Sum: $sum"
