34617f3328fd66331bb5c346a8c48c355c5a09f2
Sreedhar <ksridhar757@gmail.com>
2025-01-21 20:09:17 +0530

    this is commit

--- Read 4 Digit.sh
+++ Read 4 Digit.sh
@@ -2,9 +2,10 @@
 
 read -p "enter the 4digit number:" N
 
-a=$(echo $n | cut -c 1)
-b=$(echo $n | cut -c 2)
-c=$(echo $n | cut -c 3)
-d=$(echo $n | cut -c 4)
+a=$( echo $N | cut -c 1)
+b=$( echo $N | cut -c 2)
+c=$( echo $N | cut -c 3)
+d=$( echo $N | cut -c 4)
 
 echo "The sum of 4 digits:$[a+b+c+d]"
+
