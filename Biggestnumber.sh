726894bf8cd7a6e701aa4da013c70f28e15139b0
Sreedhar <ksridhar757@gmail.com>
2025-01-22 13:08:44 +0530

    this is commit

---
+++ Biggestnumber.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+read -p "Enter the first number:" A
+read -p "Enter the second number:" B
+read -p "Enter the third number:" C
+
+if [ $A -gt $B -a $A -gt $C ]; then
+        echo "The biggest Number is: $A"
+elif [ $B -gt $C ]; then
+        echo "The biggest number is:$B"
+else
+    echo "THe biggest number is :$C"
+
+    fi
+    
