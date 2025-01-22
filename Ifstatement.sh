8b04251eac9ab682d05f36c254f6ce34f5005a22
Sreedhar <ksridhar757@gmail.com>
2025-01-22 10:22:51 +0530

    this is commit

--- Ifstatement.sh
+++ Ifstatement.sh
@@ -1,7 +1,9 @@
-#! /bin/bash
-read -p "Enter Your Name of the dept:" dept
-if [ $dept = "sales" ]
+#! bin/bash
+read -p "Enter the dept Name:" Dept
+if [ $dept = "sales"]
 then
-echo "you should work from office"
+echo "you need to work from office"
 fi
-echo "you can work from the home"
+echo "you can work from home"
+
+
