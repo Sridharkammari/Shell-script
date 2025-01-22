d748e35a10b00d7fa9fa0959211f11fd130acc1a
Sreedhar <ksridhar757@gmail.com>
2025-01-22 10:29:25 +0530

    this is commit

--- Ifstatement.sh
+++ Ifstatement.sh
@@ -2,7 +2,8 @@
 
 read -p "Enter the dept Name:" Dept
 
-if [ "$Dept" = "sales" ]; then
+if [ "$Dept" = "sales" ] 
+then
   echo "You need to work from office."
 else
   echo "You can work from home."
