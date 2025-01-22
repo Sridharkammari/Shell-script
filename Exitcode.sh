12ca52e6cdc9a749f03c9e1a961bcfbb7ef05f24
Sreedhar <ksridhar757@gmail.com>
2025-01-22 14:45:40 +0530

    this is commit

--- Exitcode.sh
+++ Exitcode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "Enter the number from 0 to 9:" N
-if [ $n -ge 10 ]; then
+if [ $N -ge 10 ]; then
   echo "The enter number is invalied"
 else
    echo "The enter number is valied"
