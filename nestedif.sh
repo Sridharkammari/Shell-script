ee11d57a0fd4ed0d76a95c8a736ef70e683c5fb9
Sreedhar <ksridhar757@gmail.com>
2025-01-22 11:39:47 +0530

    this is commit

--- nestedif.sh
+++ nestedif.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 read -p "Enter the first Number:" A
 read -p "Enter the second Number:" B
-if [$A -eq $B ]; then
-       echo "THe bothe number are Equal."
+if [ $A -eq $B ]; then
+       echo "The both number are Equal."
 elif [ $A -gt $B ]; then
        echo "The $A is bigger than $B"
     else
        echo "THe $B is bigger thean $A"
   fi
-          
