061ed76758dd3086fc20586c7770c70c625d8497
Sreedhar <ksridhar757@gmail.com>
2025-01-22 08:43:26 +0530

    this is commit

--- Employeesal.sh
+++ Employeesal.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-read -p "Enter Employee Monthly Salary:" salary
-annual_salary=$([salary*12] | bc)
-bonus=$([annual_salary*25/100] | bc)
-echo "THe annual_salary:$(annual_salary)"
-echo "The Bonus:$bonus"
+
+read -p "Enter Employee Monthly Salary: " salary
+annual_salary=$(echo "$salary * 12" | bc)
+bonus=$(echo "$annual_salary * 0.25" | bc)
+echo "THe annual_salary: $annual_salary"
+echo "The Bonus: $bonus"
