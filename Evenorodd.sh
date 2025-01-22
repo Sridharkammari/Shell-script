d2c9724d5c973b15dca3e9ec7c8d9e69e267adcf
Sreedhar <ksridhar757@gmail.com>
2025-01-22 14:06:26 +0530

    this is commit

--- Evenorodd.sh
+++ Evenorodd.sh
@@ -1,7 +1,7 @@
 #write a program to check even or odd number
 #!/bin/bash
 read -p " enter the number:" N
-if [ [$N%2] = 0 ];then
+if [ [$N%2] -eq 0 ];then
    echo "The Given Number is Even"
 else
    echo "THe Given Number is Odd"  
