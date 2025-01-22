1e1cef6610710737df4d7ea92251b479d5eed69c
Sreedhar <ksridhar757@gmail.com>
2025-01-22 11:24:08 +0530

    this is commit

--- ifelse.sh
+++ ifelse.sh
@@ -2,10 +2,9 @@
 
 read -p " Enter the first Number:" A
 read -p " Enter the second Number:" B
-if [ $A -gt $ B ]; then
+if [ $A -gt $B ]; then
      echo "The bigger Number is: $A"
 else
     echo "THe bigger Number is" $B"
 
 fi
-  
