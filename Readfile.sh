0d0e6a02d91984b0f05632af612a252f88b97dda
Sreedhar <ksridhar757@gmail.com>
2025-01-26 18:49:52 +0530

    thisshs

--- Readfile.sh
+++ Readfile.sh
@@ -1,8 +1,9 @@
 #Readingfile if exist or not
 #!/bin/bash
 read -p "Enter the file Name to check:" fname
-if (-e $fname); then
+if [ -e $fname ]; then
 echo "The file is exist"
 else
-echo "please crete the file"
+touch $fname
+echo "please file has been created"
 fi
---
+++ checkrootuser.sh
@@ -0,0 +1,11 @@
+#checking login user is root user or not 
+#!/bin/bash
+if [ $whoami = "root"];then
+   ps -aux | head -n 6
+else
+   echo "please login as root user"
+
+fi
+
+
+
