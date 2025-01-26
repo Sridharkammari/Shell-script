0cf9bbfbb0aff8539f2e6314509a059cf1b17369
Sreedhar <ksridhar757@gmail.com>
2025-01-26 18:51:58 +0530

    thisshs

--- checkrootuser.sh
+++ checkrootuser.sh
@@ -1,7 +1,7 @@
 #checking login user is root user or not 
 #!/bin/bash
 if [ $whoami = "root" ];then
-   ps -aux | head -n 6
+   echo "ps -aux | head -n 6"
 else
    echo "please login as root user"
 
